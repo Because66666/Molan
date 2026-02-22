59d2044b9cf75e47a5238db11035a6ed0446fe83
30266-Official <a30266@qq.com>
2026-02-22 20:28:51 +0800

    添加顶部栏响应式设计

--- app/error.vue
+++ app/error.vue
@@ -1,10 +1,12 @@
 <script setup lang="ts">
 import BottomBar from "./layouts/bottom_bar.vue"
+import TopBar from "./layouts/top_bar.vue"
 import type { NuxtError } from '#app'
 const props = defineProps<{ error: NuxtError }>()
 </script>
 <template>
-    <h1>{{ error.status }}</h1>
-    <NuxtLink to="/">返回主页</NuxtLink>
+  <TopBar />
+  <h1>{{ error.status }}</h1>
+  <NuxtLink to="/">返回主页</NuxtLink>
   <BottomBar />
 </template>
