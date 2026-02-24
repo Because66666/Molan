1520f2b8489573f8d3ca163edca28e609c781a6f
30266-Official <a30266@qq.com>
2026-02-24 19:33:08 +0800

    添加了文章

--- app/pages/about.vue
+++ app/pages/about.vue
@@ -3,8 +3,8 @@
         <h1 class="section-title">关于我们</h1>
 
         <div class="card mt-6">
-            <p class="text-gray-600">抹岚报社是RIA、审判之锤社群的非盈利社团，发行有社群内部月刊杂志《方圆》、不定期刊物《莉亚晚报》。</p>
-            <p class="text-gray-600">本网站使用Nuxt.js 3框架构建，内容管理系统为Content Collections。网站代码仓库位于<NuxtLink
+            <p class="text-gray-600">抹岚报社是RIA、审判之锤社群的非盈利社团，发行有社群内部月刊杂志《方圆》、不定期刊物《莉亚晚报》。该网站亦为非盈利用途。</p>
+            <p class="text-gray-600">本网站由30266使用Github Copilot、OpenCode等AI工具构建。网站框架为Nuxt.js 4，内容管理系统为@nuxt/content，使用vue-pdf-embed进行pdf嵌入式渲染。网站代码仓库位于<NuxtLink
                     to="https://github.com/30266-Official/Molan" target="_blank">https://github.com/30266-Official/Molan
                 </NuxtLink>。</p>
             <p class="text-gray-600">本网站使用字体为汉仪玄宋，这是专有字体，但可以免费用作非商业用途。</p>
